925aaa65177fe81ad5f92e2a065b74d86dd0b3bb
degski <degski@gmail.com>
2019-04-02 14:48:22 +0300

    re-correct, i.e. revert, bogus casting

--- Mado/Hexcontainer.hpp
+++ Mado/Hexcontainer.hpp
@@ -98,7 +98,7 @@ struct RadiusBase {
 
     [[ nodiscard ]] static constexpr bool is_invalid ( const size_type q_, const size_type r_ ) noexcept {
         auto abs = [ ] ( auto const a ) noexcept { return a > decltype ( a ) { 0 } ? a : -a; };
-        return abs ( q_ - static_cast<size_type> ( zero_base ? 0 : radius ( ) ) ) > static_cast<size_type> ( radius ( ) ) or abs ( r_ - static_cast<size_type> ( zero_base ? 0 : radius ( ) ) ) > static_cast<size_type> ( radius ( ) ) or abs ( -q_ - r_ + ( 2 * static_cast<size_type> ( zero_base ? 0 : radius ( ) ) ) ) > static_cast<size_type> ( radius ( ) );
+        return abs ( q_ - ( zero_base ? 0 : radius ( ) ) ) > radius ( ) or abs ( r_ - ( zero_base ? 0 : radius ( ) ) ) > radius ( ) or abs ( -q_ - r_ + ( 2 * ( zero_base ? 0 : radius ( ) ) ) ) > radius ( );
     }
     [[ nodiscard ]] static constexpr bool is_valid ( const size_type q_, const size_type r_ ) noexcept {
         return not ( is_invalid ( q_, r_ ) );
