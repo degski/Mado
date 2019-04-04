a424f029a2eb0032551f47d3bc119905b363ea6a
degski <degski@gmail.com>
2019-04-04 10:02:11 +0300

    base case for testing

--- Mado/main.cpp
+++ Mado/main.cpp
@@ -58,7 +58,7 @@
 #include "../../MCTSSearchTree/include/flat_search_tree.hpp"
 
 
-#if 1
+#if 0
 
 #include "App.hpp"
 
@@ -237,16 +237,15 @@ struct Hash32 {
 
 int main ( ) {
 
-    int size = 0;
-    Mado<3> state;
+    Mado<5> state;
 
     plf::nanotimer timer;
 
     timer.start ( );
 
-    for ( int i = 0; i < 20'000'000; ++i ) {
+    for ( int i = 0; i < 10'000; ++i ) {
         while ( true ) {
-            auto const m = state.get_random_move ( & size );
+            auto const m = state.get_random_move ( );
             if ( state.terminal ( ) )
                 break;
             state.move_winner ( m );
@@ -254,9 +253,7 @@ int main ( ) {
         state.reset ( );
     }
 
-    std::cout << ( int ) ( timer.get_elapsed_ms ( ) / 1'000 ) << " secs" << nl;
-
-    std::cout << size << nl;
+    std::cout << ( int ) ( timer.get_elapsed_ms ( ) ) << " msecs" << nl;
 
     return EXIT_SUCCESS;
 }
